d5a154654b855ee3b3f9cb30dbf93986243a2aa1
Guilherme Santos <guilhermedev@hotmail.com>
2022-07-23 10:34:42 -0300

    add cors

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(3)->create();
+        $role = \App\Models\Role::create(["name" => "TI"]);
+        \App\Models\User::create([
+            "name" => "Guilherme Santos",
+            "email" => "guilhermedev@hotmail.com",
+            "password" => "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi",
+            "role_id" => 1
+        ]);
+
+        \App\Models\Permission::create(["name" => "dashboard.show"]);
+        \App\Models\Permission::create(["name" => "attendance.show"]);
+        \App\Models\Permission::create(["name" => "role.show"]);
+        \App\Models\Permission::create(["name" => "permission"]);
+        \App\Models\Permission::create(["name" => "client.show"]);
+        \App\Models\Permission::create(["name" => "employee.show"]);
+
+        $role->permissions()->attach(['1', '2', '3', '4', '5', '6']);
+        $role->save();
     }
 }
